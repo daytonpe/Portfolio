28774a7efcb19bc98ff5e59d5a74f95452165311
Pat Dayton <daytonpe@Pats-MacBook-Pro.local>
2018-08-29 22:53:17 -0500

    update fonts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,29 +5,29 @@ import Experience from './Experience'
 class Contact extends React.Component {
   render() {
     return (
-      <div class="section" id="contactDiv">
+      <div className="section" id="contactDiv">
         <h2>Contact</h2>
-        <div class="row">
-          <div class="col col-md-4 contactCol">
+        <div className="row">
+          <div className="col col-md-4 contactCol">
             <h4>I live in Dallas, Texas.</h4>
           </div>
-          <div class="col col-md-4 contactCol">
+          <div className="col col-md-4 contactCol">
             <h4>Feel free to reach out.</h4>
           </div>
-          <div class="col col-md-4 contactCol">
+          <div className="col col-md-4 contactCol">
             <h4>Let's chat over <a target="_blank" href="https://www.houndstoothcoffee.com/">coffee</a>.</h4>
           </div>
         </div>
 
         <div id="contactIconDiv">
-          <a class="contactAnchor" target="_blank" href="mailto:pat@smalltowntopcollege.com">
-            <i class="fa fa-envelope fa-5x contactIcon" aria-hidden="true"></i>
+          <a className="contactAnchor" target="_blank" href="mailto:pat@smalltowntopcollege.com">
+            <i className="fa fa-envelope fa-5x contactIcon" aria-hidden="true"></i>
           </a>
-          <a class="contactAnchor" target="_blank" href="https://www.linkedin.com/in/pat-dayton-0514413a/">
-            <i class="fa fa-linkedin-square fa-5x contactIcon" aria-hidden="true"></i>
+          <a className="contactAnchor" target="_blank" href="https://www.linkedin.com/in/pat-dayton-0514413a/">
+            <i className="fa fa-linkedin-square fa-5x contactIcon" aria-hidden="true"></i>
           </a>
-          <a class="contactAnchor" target="_blank" href="https://github.com/daytonpe">
-            <i class="fa fa-github-square fa-5x contactIcon" aria-hidden="true"></i>
+          <a className="contactAnchor" target="_blank" href="https://github.com/daytonpe">
+            <i className="fa fa-github-square fa-5x contactIcon" aria-hidden="true"></i>
           </a>
         </div>
       </div>
--- src/components/Education.js
+++ src/components/Education.js
@@ -4,25 +4,25 @@ import { render } from 'react-dom';
 class Education extends React.Component {
   render() {
     return (
-      <div class="section" id="educationDiv">
+      <div className="section" id="educationDiv">
 				<div id="eduContent">
 					<h2>Education</h2>
-					<div class="row educationRow">
-						<div class="col col-md-4 logoCol">
-							<img class="eduLogo" src="/wp-content/themes/twentyfifteen-child/img/mit.png" alt=""/>
+					<div className="row educationRow">
+						<div className="col col-md-4 logoCol">
+							<img className="eduLogo" src="/wp-content/themes/twentyfifteen-child/img/mit.png" alt=""/>
 						</div>
-						<div class="col col-md-8">
+						<div className="col col-md-8">
 							<a target="_blank" href="http://meche.mit.edu/"><h4>Massachusetts Intstitute of Technology</h4></a>
 							<h5>B.S. Mechanical Engineering</h5>
 							<p>My time at the Institute was the most influential in my life. MIT brings together an vastly diverse student body to learn and tackle incredibly difficult problems together. My studies in mechanical engineering were highly focused on the product design side of the major, culminating in the construction of Glow (see above).</p>
               <p>Along with my studies I was lucky enough to play four years of varsity baseball, competing in the D3 NCAA tournament three times and raising the schools first conference championship banner my senior year.</p>
 						</div>
 					</div>
-					<div class="row educationRow">
-						<div class="col col-md-4 logoCol col-md-push-8">
-							<img class="eduLogo" src="/wp-content/themes/twentyfifteen-child/img/utd.png" alt=""/>
+					<div className="row educationRow">
+						<div className="col col-md-4 logoCol col-md-push-8">
+							<img className="eduLogo" src="/wp-content/themes/twentyfifteen-child/img/utd.png" alt=""/>
 						</div>
-						<div class="col col-md-8 col-md-pull-4">
+						<div className="col col-md-8 col-md-pull-4">
 							<h4><a target="_blank" href="http://cs.utdallas.edu/" target="_blank">University of Texas at Dallas</a></h4>
 							<h5>M.S. Computer Science</h5>
 							<p>Combining my experiences in product development from MIT with my time at a technology startup, I came to the realization that the most captivating areas of growth to come in the upcoming decades will be in the land of computing. Along with working as a developer, I am pursuing a Master’s of Science in Computer Science following the Intelligent Systems Track (AI/ML) at UTDallas. I plan to graduate in the Spring of 2020.</p>
